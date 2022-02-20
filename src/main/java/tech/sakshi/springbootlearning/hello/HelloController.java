06315a3421eef8814661c63adb0a3d79fcd90221
Sakshi Choudhary <choudharysakshi023@gmail.com>
2022-02-20 14:24:15 +0530

    ⚡️ maps in HelloController

--- src/main/java/tech/sakshi/springbootlearning/hello/HelloController.java
+++ src/main/java/tech/sakshi/springbootlearning/hello/HelloController.java
@@ -1,13 +1,19 @@
 package tech.sakshi.springbootlearning.hello;
 
+import java.util.*;  
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    
-    @RequestMapping(value= "/hello")
-    public String sayHi(){
-        return "Hi";
+        
+    @RequestMapping("/hello")
+    public Map<Integer,String> sayHi(){
+        Map<Integer,String> map1=new HashMap<Integer,String>();  
+        map1.put(100,"Nate");    
+        map1.put(101,"Cassie");    
+        map1.put(102,"Rue");           
+        return map1;
     }
 }
