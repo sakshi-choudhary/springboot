83db9df17268b36975b4e45b6704563d579ff72b
Sakshi Choudhary <choudharysakshi023@gmail.com>
2022-05-08 13:37:38 +0530

    ✨ add post n put

--- src/main/java/tech/sakshi/springbootlearning/topic/TopicController.java
+++ src/main/java/tech/sakshi/springbootlearning/topic/TopicController.java
@@ -2,19 +2,37 @@ package tech.sakshi.springbootlearning.topic;
 
 import java.util.*;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TopicController {
+
+    @Autowired
+    private TopicService topicService;
     
-    @RequestMapping("/topic")
+    @RequestMapping("/topics")
     public List<Topic> getAllTopics(){
-        return Arrays.asList(
-            new Topic("life","About Life","Life is shit!"),
-            new Topic("love","About Love","Love is great!"),
-            new Topic("lie","About Lie","Hate Lies!")
-        );
+        return topicService.getAllTopics();
+    }
+
+    @RequestMapping("/topics/{id}")
+    public Topic getTopic(@PathVariable String id){
+        return topicService.getTopic(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/topics")
+    public void addTopic(@RequestBody Topic topic){
+        topicService.addTopic(topic);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/topics/{id}")
+    public void updateTopic(@PathVariable String id,@RequestBody Topic topic){
+        topicService.updateTopic(id,topic);
     }
 
 }
---
+++ src/main/java/tech/sakshi/springbootlearning/topic/TopicService.java
@@ -0,0 +1,44 @@
+package tech.sakshi.springbootlearning.topic;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+    
+    private List<Topic> topics = new ArrayList<>(Arrays.asList(
+            new Topic("life","About Life","Life is shit!"),
+            new Topic("love","About Love","Love is great!"),
+            new Topic("lie","About Lie","Hate Lies!")
+        ));
+
+    public List<Topic> getAllTopics(){
+        return topics;
+    }
+
+    public Topic getTopic(String id){
+        return topics.stream().filter(t->t.getId().equals(id)).findFirst().get();
+    }
+
+    public void addTopic(Topic topic){
+        topics.add(topic);
+    }
+
+    public void updateTopic(String id,Topic topic){
+        // Topic temp=topics.stream().filter(t->t.getId().equals(id)).findFirst().get();
+        // topics.add(topic);
+        // topics.remove(temp);
+
+        for(int i=0;i<topics.size();i++){
+            Topic t=topics.get(i);
+            if(t.getId().equals(id)){
+                topics.set(i,topic);
+                return;
+            }
+        }
+    }
+
+}
